99a9af6d07d00b87a87c2bedda09c0e84a0e843b
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-08-27 16:22:30 +0200

    test interface

--- script.js
+++ script.js
@@ -7,16 +7,35 @@ function getComputerChoice() {
     return randomElement;
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter your choice").toUpperCase();
-    if(choice === "PAPER" || choice === "ROCK" || choice === "SCISSOR"){
-        return choice;
-    }
-    else{
-        alert("Wrong input, only paper, rock or scissor is valid.")
-        return getHumanChoice();
-    }
-}
+// function getHumanChoice() {
+//     let choice = prompt("Enter your choice").toUpperCase();
+//     if(choice === "PAPER" || choice === "ROCK" || choice === "SCISSOR"){
+//         return choice;
+//     }
+//     else{
+//         alert("Wrong input, only paper, rock or scissor is valid.")
+//         return getHumanChoice();
+//     }
+// }
+
+
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+  playRound('ROCK', getComputerChoice())
+});
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+  playRound('PAPER', getComputerChoice())
+});
+const scissor = document.querySelector("#scissor");
+scissor.addEventListener("click", () => {
+  playRound('SCISSOR', getComputerChoice())
+});
+
+
+
+
+
 function playRound(getHumanChoice, getComputerChoice) {
 
     console.log("Your choice: " + getHumanChoice);
@@ -60,28 +79,27 @@ function playRound(getHumanChoice, getComputerChoice) {
 
 }
 
-function playGame() {
+// function playGame() {
 
-    for (i = 0; i < 5; i++) {
+//     for (i = 0; i < 5; i++) {
 
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
 
-    }
+//     }
 
-    console.log("Score: " + humanScore + " : " + computerScore);
+//     console.log("Score: " + humanScore + " : " + computerScore);
 
-    if (humanScore > computerScore) {
-        console.log("Congratulations! You win!")
-    } else if (computerScore > humanScore) {
-        console.log("Better luck next time. You lose!")
-    } else {
-        console.log("No one wins. It is tied!")
-    }
+//     if (humanScore > computerScore) {
+//         console.log("Congratulations! You win!")
+//     } else if (computerScore > humanScore) {
+//         console.log("Better luck next time. You lose!")
+//     } else {
+//         console.log("No one wins. It is tied!")
+//     }
 
-}
+// }
 
-playGame();
-
-
+// playGame();
+//
